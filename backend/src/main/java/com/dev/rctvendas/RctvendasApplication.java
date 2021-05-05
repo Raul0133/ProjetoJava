9079766fbdb4d4c2844e8bd450d266282821cda9
Raul Caldas <raullek03@gmail.com>
2021-05-05 13:14:08 -0300

    Projected Created

---
+++ backend/src/main/java/com/dev/rctvendas/RctvendasApplication.java
@@ -0,0 +1,13 @@
+package com.dev.rctvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RctvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RctvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dev/rctvendas/RctvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.rctvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RctvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
