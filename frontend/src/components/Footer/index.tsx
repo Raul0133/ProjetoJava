7ff579c2a2d37c0f91ab3ac8eab6b452d63f728a
Raul Caldas <raullek03@gmail.com>
2021-05-05 21:15:57 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/Raul0133" target="_blank" rel="noreferrer">Raul Caldas</a></p>
+          <p className="text-light"><small><strong>Spring React</strong><br/>
+            Trabalho De Java: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@_raulcaldas</a></small></p>
+        </div>
+      </footer>
+    );
+}
+
+export default Footer;
